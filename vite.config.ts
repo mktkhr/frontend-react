b97b835d29b4a79a9c25544a6f09119850bc09a7
EMS engineering <j39g8bxh@gmail.com>
2025-05-18 19:33:22 +0900

    [update] #1 proxy設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "node:path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    proxy: {
+      api: {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
 });
